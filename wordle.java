6b85cdc495ca2f49c584cdf9309e030e13e9cf59
Magno Junior <magnobrito00@gmail.com>
2022-10-21 14:21:07 -0300

    My first commit on this game

---
+++ wordle.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class wordle {
+
+    /*
+     * The main purpose of this game is to guess the word according to its letters
+     */
+
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+
+    }
+
+}
