3940753c5315df7b903364bcfaf25626dca72340
Magno Junior <magnobrito00@gmail.com>
2022-10-23 21:08:00 -0300

    Random function

--- wordle.java
+++ wordle.java
@@ -1,9 +1,9 @@
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 public class wordle {
 
@@ -11,8 +11,20 @@ public class wordle {
      * The main purpose of this game is to guess the word according to its letters
      */
 
-    public static void main(String[] args) {
-        List<String> words = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> file = new ArrayList<>();
+        Scanner sc = new Scanner(
+                new FileReader("C:\\Users\\Radagast\\Desktop\\CODIGOS\\Java\\DIO-BOOTCAMP\\words.txt"));
+        String words;
+        while (sc.hasNext()) {
+            words = sc.next();
+            file.add(words);
+
+        }
+        String[] array = file.toArray(new String[0]);
+        Random rand = new Random();
+        int random_number = rand.nextInt(array.length - 1);
+        System.out.println(array[random_number]);
 
     }
 
