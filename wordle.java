f9bde071fec2f188dc92afdd6a6e04bc3affeda0
Magno Junior <magnobrito00@gmail.com>
2022-10-24 08:20:49 -0300

    Continuing the game

--- wordle.java
+++ wordle.java
@@ -9,6 +9,7 @@ public class wordle {
 
     /*
      * The main purpose of this game is to guess the word according to its letters
+     * The user has six chances to guess the hiding word.
      */
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -19,12 +20,22 @@ public class wordle {
         while (sc.hasNext()) {
             words = sc.next();
             file.add(words);
-
         }
         String[] array = file.toArray(new String[0]);
         Random rand = new Random();
         int random_number = rand.nextInt(array.length - 1);
-        System.out.println(array[random_number]);
+        String missing_word = array[random_number];
+
+        Character[] guess = new Character[5];
+        for (int i = 0; i < 5; i++) {
+            guess[i] = missing_word.charAt(i);
+            System.out.println(guess[i]);
+        }
+
+        // The game
+        System.out.println("----------------------Welcome to the game Wordle-----------------------");
+        System.out.println("----You have six chances to guess the missing word with five letters---");
+        System.out.println("-----------------------------------------------------------------------");
 
     }
 
